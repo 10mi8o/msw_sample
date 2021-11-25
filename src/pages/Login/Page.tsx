1d21fd02f3ae15e8b51f7a9c202f445cd2246146
10mi8o <totoma855@gmail.com>
2021-11-25 09:34:49 +0900

    パス名変更

--- src/pages/Login/Page.tsx
+++ src/pages/Login/Page.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
   const navigate = useNavigate();
-  const HOME_PAGE = "/HOME";
+  const HOME_PAGE = "/home";
 
   const login = () => {
     fetch("http://localhost:3000/login", { method: "POST" }).then(() =>
